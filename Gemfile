b3c83fb5fbdf61cb96ad099649aeaa6e6f06603d
Rocco Janse <rocco.janse@efocus.nl>
2016-06-03 12:37:35 +0200

    Added normalize css reset stylesheet

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source "https://rubygems.org"
+
+gem 'support-for', '~> 1.0'
---
+++ config.rb
@@ -0,0 +1,7 @@
+require 'support-for'
+
+http_path = "/"
+css_dir = "css"
+sass_dir = "src/scss"
+images_dir = "assets/img"
+javascripts_dir = "js"
