489019c4d740987638868099e18e0d45b4325abe
Rocco Janse <rocco.janse@efocus.nl>
2016-11-02 17:12:19 +0100

    Clean start

--- Gemfile
+++
@@ -1,3 +0,0 @@
-source "https://rubygems.org"
-
-gem 'support-for', '~> 1.0'
--- config.rb
+++
@@ -1,7 +0,0 @@
-require 'support-for'
-
-http_path = "/"
-css_dir = "css"
-sass_dir = "src/scss"
-images_dir = "assets/img"
-javascripts_dir = "js"
